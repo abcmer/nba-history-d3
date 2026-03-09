8c319b7d792978b79dbb2fd632f47314672ef70a
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2026-03-09 14:08:42 -0500

    Add slider and mobile enhancements with recent data (#3)
    * add claude file
    
    * added most recent data
    
    * add slider, play/pause, team colors
    
    * mobile enhancements

--- src/components/NbaViz.tsx
+++ src/components/NbaViz.tsx
@@ -58,27 +58,22 @@ class Viz {
   }
 
   checkForMobile(): boolean {
-    let check = false;
-    const a = navigator.userAgent || navigator.vendor || (window as Window & { opera?: string }).opera || '';
-    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(String(a).substring(0, 4))) {
-      check = true;
-    }
-    return check;
+    return window.innerWidth < 768;
   }
 
   defineProperties(): void {
     if (this.isMobile) {
       this.props = {
-        barHeight: 50,
-        yTickFontSize: '24',
-        chartMarginLeft: 0,
-        chartMarginRight: 0,
-        chartMarginUp: 0,
-        chartMarginDown: 200,
-        chartTitleFontSize: '1.7rem',
+        barHeight: 20,
+        yTickFontSize: '13',
+        chartMarginLeft: 5,
+        chartMarginRight: 5,
+        chartMarginUp: 10,
+        chartMarginDown: 110,
+        chartTitleFontSize: '0.85rem',
         width: window.innerWidth,
         height: window.innerHeight,
-        chartWidth: window.innerWidth,
+        chartWidth: window.innerWidth - 10,
       };
     } else {
       this.props = {
@@ -156,13 +151,18 @@ class Viz {
       yAxisTicks: {},
       yearSlider: this.addGroup({
         class: 'yearSlider',
-        xOffset: this.props.chartWidth * 0.1,
+        // On mobile, push slider right enough to clear the play button (≈60px)
+        xOffset: this.isMobile
+          ? Math.max(60, this.props.chartWidth * 0.1)
+          : this.props.chartWidth * 0.1,
         yOffset: this.props.height - this.props.chartMarginDown / 2,
       }),
       yearSliderKnob: null as unknown as d3.Selection<SVGGElement, unknown, HTMLElement, unknown>,
       chartTitle: this.addGroup({
         class: 'chartTitle',
-        xOffset: this.props.width - this.props.chartMarginRight - 440,
+        xOffset: this.isMobile
+          ? this.props.width / 2
+          : this.props.width - this.props.chartMarginRight - 440,
         yOffset: this.props.chartMarginUp + 30,
       }),
     };
@@ -171,8 +171,9 @@ class Viz {
   addChartTitle(): void {
     this.layers.chartTitle
       .append('text')
-      .text('NBA Championship Teams Over Time')
-      .attr('font-size', this.props.chartTitleFontSize);
+      .text(this.isMobile ? 'NBA Championships' : 'NBA Championship Teams Over Time')
+      .attr('font-size', this.props.chartTitleFontSize)
+      .attr('text-anchor', this.isMobile ? 'middle' : 'start');
   }
 
   addZeroCoordinate(): void {
@@ -192,10 +193,18 @@ class Viz {
       .range([0, this.props.width - this.props.chartMarginRight - this.props.chartMarginLeft]);
   }
 
+  get sliderTrackWidth(): number {
+    if (this.isMobile) {
+      const sliderOriginX = Math.max(60, this.props.chartWidth * 0.1);
+      return this.props.chartWidth - sliderOriginX - this.props.chartMarginRight;
+    }
+    return this.props.chartWidth * 0.9;
+  }
+
   createYearsToHorizontalPixelsScale(): void {
     this.yearsToHorizontalPixelsScale = d3.scaleLinear()
       .domain([1947, 2025])
-      .range([10, this.props.chartWidth * 0.9 - 10]);
+      .range([10, this.sliderTrackWidth - 10]);
   }
 
   addAxisLines(): void {
@@ -221,6 +230,11 @@ class Viz {
 
   addXaxisTicks(options: { titlesMax: number }): void {
     const titlesMax = options.titlesMax;
+    // Skip labels that would overlap: require at least 22px between them
+    const pixelsPerTick = this.titlesToPixelsScale(1);
+    const labelStep = Math.max(1, Math.ceil(22 / pixelsPerTick));
+    const labelFontSize = this.isMobile ? '11px' : '16px';
+
     for (let i = 1; i <= titlesMax; i++) {
       this.layers.xAxisTicks[i] = this.layers.axis
         .append('g')
@@ -236,12 +250,15 @@ class Viz {
         .style('stroke', 'black')
         .style('stroke-width', 2);
 
-      this.layers.xAxisTicks[i]
-        .append('text')
-        .text(i)
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline', 'middle')
-        .attr('y', 20);
+      if (i % labelStep === 0) {
+        this.layers.xAxisTicks[i]
+          .append('text')
+          .text(i)
+          .attr('text-anchor', 'middle')
+          .attr('alignment-baseline', 'middle')
+          .attr('font-size', labelFontSize)
+          .attr('y', 20);
+      }
     }
   }
 
@@ -271,14 +288,14 @@ class Viz {
     this.layers.yearSlider
       .append('line')
       .attr('x1', 0)
-      .attr('x2', this.props.chartWidth * 0.9)
+      .attr('x2', this.sliderTrackWidth)
       .style('stroke', 'black')
       .style('stroke-width', 2);
 
     this.layers.yearSlider
       .append('line')
-      .attr('x1', this.props.chartWidth * 0.9)
-      .attr('x2', this.props.chartWidth * 0.9)
+      .attr('x1', this.sliderTrackWidth)
+      .attr('x2', this.sliderTrackWidth)
       .attr('y1', 10)
       .attr('y2', -10)
       .attr('stroke', 'black')
@@ -296,11 +313,12 @@ class Viz {
       .attr('y', 25)
       .text(options.year)
       .attr('text-anchor', 'middle')
-      .attr('alignment-baseline', 'middle');
+      .attr('alignment-baseline', 'middle')
+      .attr('font-size', this.isMobile ? '13px' : '16px');
 
     this.layers.yearSliderKnob
       .append('circle')
-      .attr('r', 10)
+      .attr('r', this.isMobile ? 16 : 10)
       .style('fill', 'white')
       .style('stroke-width', 3)
       .style('stroke', 'black');
@@ -369,30 +387,32 @@ class Viz {
         if (self.onPlayPauseToggle) self.onPlayPauseToggle();
       });
 
+    const r = this.isMobile ? 20 : 18;
     btnGroup.append('rect')
-      .attr('x', -18)
-      .attr('y', -18)
-      .attr('width', 36)
-      .attr('height', 36)
+      .attr('x', -r)
+      .attr('y', -r)
+      .attr('width', r * 2)
+      .attr('height', r * 2)
       .attr('rx', 4)
       .style('fill', 'white')
       .style('stroke', '#C40628')
       .style('stroke-width', 2.5);
 
+    const s = r / 18; // scale factor for icons
     if (this.isPlaying) {
       // Pause icon: two vertical bars
       btnGroup.append('rect')
-        .attr('x', -9).attr('y', -10)
-        .attr('width', 7).attr('height', 20)
+        .attr('x', -9 * s).attr('y', -10 * s)
+        .attr('width', 7 * s).attr('height', 20 * s)
         .style('fill', '#C40628');
       btnGroup.append('rect')
-        .attr('x', 2).attr('y', -10)
-        .attr('width', 7).attr('height', 20)
+        .attr('x', 2 * s).attr('y', -10 * s)
+        .attr('width', 7 * s).attr('height', 20 * s)
         .style('fill', '#C40628');
     } else {
       // Play icon: triangle
       btnGroup.append('polygon')
-        .attr('points', '-7,-12 15,0 -7,12')
+        .attr('points', `${-7 * s},${-12 * s} ${15 * s},0 ${-7 * s},${12 * s}`)
         .style('fill', '#C40628');
     }
   }
